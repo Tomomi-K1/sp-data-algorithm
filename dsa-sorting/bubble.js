5553a79b5a0581f22604faad4f4072bc660431ee
Tomomi-K1 <tomomi.music@gmail.com>
2023-05-26 15:09:00 -0700

    mergeSort done

--- dsa-sorting/bubble.js
+++ dsa-sorting/bubble.js
@@ -1,8 +1,8 @@
 function bubbleSort(arr) {
+    let swapped;
     for(i = 0; i< arr.length; i++){
-        let swapped = false;
+        swapped = false;
         for(j = 0; j <arr.length-i; j++){
-            swapped=false;
             if(arr[j] > arr[j+1]){
                 let temp = arr[j+1];
                 arr[j+1] = arr[j];
@@ -12,9 +12,28 @@ function bubbleSort(arr) {
         }
         if(!swapped) break;
     }
+    
     return arr;
 
 }
+
+// function bubbleSort(arr) {
+//     for(i = 0; i< arr.length; i++){
+//         // let swapped = false;
+//         for(j = 0; j <arr.length-i; j++){
+//             // swapped=false;
+//             if(arr[j] > arr[j+1]){
+//                 let temp = arr[j+1];
+//                 arr[j+1] = arr[j];
+//                 arr[j] = temp;
+//                 // swapped=true;
+//             }  
+//         }
+//         // if(!swapped) break;
+//     }
+//     return arr;
+
+// }
 
 module.exports = bubbleSort;
 
